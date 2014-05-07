e8156aea00d45fb804901facf5cfad562c734909
Peter Markou <markoupetr@gmail.com>
2014-05-07 10:11:42 +0300

    Remove redundant  action in SessionsController

--- signup-before/app/controllers/sessions_controller.rb
+++ signup-before/app/controllers/sessions_controller.rb
@@ -1,8 +1,4 @@
 class SessionsController < ApplicationController
-  def new
-
-  end
-
   def create
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
