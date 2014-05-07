dab1891ffcd6d04b3ba6c55d3efac0e26b3b555f
Peter Markou <markoupetr@gmail.com>
2014-05-07 12:29:33 +0300

    Add controller tests

--- signup-before/test/controllers/passwords_controller_test.rb
+++ signup-before/test/controllers/passwords_controller_test.rb
@@ -1,14 +1,30 @@
 require 'test_helper'
 
 class PasswordsControllerTest < ActionController::TestCase
-  test "should get new" do
+  test "should get the password changing page" do
     get :new
+    password_form = assigns(:password_form)
+
     assert_response :success
+    assert_not_nil password_form
+    assert_kind_of PasswordForm, password_form
   end
 
-  test "should get create" do
-    get :create
-    assert_response :success
+  test "should change the password" do
+    login_as users(:peter)
+
+    post :create, :password_form =>
+                  { 
+                    :original_password => "secret",
+                    :new_password => "37emzf69",
+                    :new_password_confirmation => "37emzf69"
+                  }
+    password_form = assigns(:password_form)
+
+    assert_not_nil password_form
+    assert_kind_of PasswordForm, password_form
+    assert_redirected_to users(:peter)
+    assert_equal "Successfully changed password.", flash[:notice]
   end
 
 end
--- signup-before/test/controllers/users_controller_test.rb
+++ signup-before/test/controllers/users_controller_test.rb
@@ -1,19 +1,49 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  test "should get new" do
+  test "should get the signup page" do
     get :new
+    signup_form = assigns(:signup_form)
+
     assert_response :success
+    assert_not_nil signup_form
+    assert_kind_of SignupForm, signup_form
   end
 
-  test "should get create" do
-    get :create
-    assert_response :success
+  test "should redirect to user's profile after successful signup" do
+    post :create, :user =>
+                  {
+                    :username => "mpeter",
+                    :email => "markoupetr@gmail.com",
+                    :password => "37emzf69",
+                    :password_confirmation => "37emzf69",
+                    :twitter_name => "m-peter",
+                    :github_name => "m-peter",
+                    :bio => "Currently a GSoC student."
+                  }
+    signup_form = assigns(:signup_form)
+
+    assert_not_nil signup_form
+    assert_not_nil session[:user_id]
+    assert_kind_of SignupForm, signup_form
+    assert_redirected_to signup_form.user
+    assert_equal "Thank you for signing up!", flash[:notice]
   end
 
-  test "should get show" do
-    get :show
+  test "should show logged in user's profile" do
+    peter = users(:peter)
+    login_as peter
+    get :show, {'id' => peter}
+    
+    logged_in_user = assigns(:user)
+
     assert_response :success
+    assert_not_nil logged_in_user
+    assert_equal peter.email, logged_in_user.email
+    assert_equal peter.username, logged_in_user.username
+    assert_equal peter.profile.twitter_name, logged_in_user.profile.twitter_name
+    assert_equal peter.profile.github_name, logged_in_user.profile.github_name
+    assert_equal peter.profile.bio, logged_in_user.profile.bio
   end
 
 end
