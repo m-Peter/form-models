315e9c5e51fa3b300a31de17ed2769a315e8644d
Peter Markou <markoupetr@gmail.com>
2014-05-17 19:56:37 +0300

    Populate the root model with its attributes

--- user_with_email/app/forms/form_object/model_factory.rb
+++ user_with_email/app/forms/form_object/model_factory.rb
@@ -11,7 +11,17 @@ module FormObject
       @model = create_root_model
     end
 
-    private
+    def root_populator_args
+      root_populator_args = {
+        :model => model,
+        :attrs => params_for_current_scope(attributes.values.first),
+        :association_name => attributes.keys.first
+      }
+    end
+
+    def params_for_current_scope(attrs)
+      attrs.dup.reject { |_, v| v.is_a?(Hash) }
+    end
 
     def create_root_model
       model_class = attributes.keys.first.to_s.camelize.constantize
--- user_with_email/test/forms/model_factory_test.rb
+++ user_with_email/test/forms/model_factory_test.rb
@@ -5,7 +5,7 @@ class ModelFactoryTest < ActiveSupport::TestCase
     @params = {
       user: {
         name: "Petros",
-        age: 22,
+        age: 23,
         gender: 0,
         email_attributes: {
           address: "cs3199@teilar.gr"
@@ -26,7 +26,22 @@ class ModelFactoryTest < ActiveSupport::TestCase
 
   test "model factory can populate the models" do
     model = @factory.populate_model
+    
     assert_kind_of User, model
     assert_kind_of User, @factory.model
+  end
+
+  test "root populator populates the root model with args" do
+    @factory.populate_model
+    args = @factory.root_populator_args
+    attrs = {
+      name: "Petros",
+      age: 23,
+      gender: 0
+    }
+
+    assert_instance_of User, args[:model]
+    assert_equal attrs, args[:attrs]
+    assert_equal :user, args[:association_name]
   end
 end
