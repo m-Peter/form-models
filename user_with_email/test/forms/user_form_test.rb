f94e7d6eac435e2dc8013e171e435a45c8853d40
Peter Markou <markoupetr@gmail.com>
2014-05-22 20:23:11 +0300

    UserForm's model gets render correctly in edit template

--- user_with_email/test/forms/user_form_test.rb
+++ user_with_email/test/forms/user_form_test.rb
@@ -97,5 +97,32 @@ class UserFormTest < ActiveSupport::TestCase
       assert_match /<\/select>/, output_buffer
       assert_match /<input name="commit" type="submit" value="Create User" \/>/, output_buffer
     end
+
+    test "form_for renders the values of persisted model" do
+      user = User.create!(name: "Petrakos", age: 23, gender: 0)
+      user_form = UserForm.new(user)
+
+      form_for user_form do |f|
+        concat f.label(:name)
+        concat f.text_field(:name)
+        concat f.label(:age)
+        concat f.number_field(:age)
+        concat f.label(:gender)
+        concat f.select(:gender, User.get_genders_dropdown)
+        concat f.submit('Update User')
+      end
+
+      id = user.id
+
+      assert_match /action="\/users\/#{id}"/, output_buffer
+      assert_match /class="edit_user"/, output_buffer
+      assert_match /id="edit_user_#{id}"/, output_buffer
+      assert_match /method="post"/, output_buffer
+      assert_match /<input name="_method" type="hidden" value="patch"/, output_buffer
+      assert_match /<input id="user_name" name="user\[name\]" type="text" value="#{user.name}" \/>/, output_buffer
+      assert_match /<input id="user_age" name="user\[age\]" type="number" value="#{user.age}" \/>/, output_buffer
+      assert_match /<option selected="selected" value="0">Male<\/option>/, output_buffer
+      assert_match /<input name="commit" type="submit" value="Update User" \/>/, output_buffer
+    end
   end
 end
