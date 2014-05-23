cc913d201b41c0351dc64653e5ebd9f2524f6e82
Peter Markou <markoupetr@gmail.com>
2014-05-23 13:26:14 +0300

    Delete the previous implementation of FormObject

--- user_with_email/app/forms/form_object/populator/has_one.rb
+++
@@ -1,9 +0,0 @@
-module FormObject
-  module Populator
-    class HasOne < Abstract
-      def assign_to_parent
-        parent.send("#{association_name}=", model)
-      end
-    end
-  end
-end
--- user_with_email/app/forms/form_object/populator/root.rb
+++
@@ -1,9 +0,0 @@
-module FormObject
-  module Populator
-    class Root < Abstract
-      def assign_to_parent
-        # no-op
-      end
-    end
-  end
-end
